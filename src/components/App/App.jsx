114fc328e82bbe58ca0f2b76cfe5aea9fcfe2795
Jiayi Hu <steph.jiayi@gmail.com>
2016-04-29 23:34:10 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+require('./main.scss');
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>This is a React Application!</div>
+    );
+  }
+}
